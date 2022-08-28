395edd5fc3ef363e3d7a46181e8cef162597d40d
Scumsy <99488401+Scumsy@users.noreply.github.com>
2022-08-28 18:03:28 +0000

    Deploying to gh-pages from @ Scumsy/goit-js-hw-09@6dffd2686674bff5301b4267acf160ab83af7f8b 🚀

---
+++ 01-color-switcher.e80e0f1c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){(o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3))&&(t.disabled=!0)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.e80e0f1c.js.map
