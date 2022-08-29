a6c965f7af32fcf397538bd74cdf155b553f930f
Scumsy <dmitry.koschavka@gmail.com>
2022-08-29 19:51:27 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,6 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
+const form = document.querySelector('form');
+form.addEventListener('submit', () => {
+  console.log('submit clicked');
+});
+console.log(form);
